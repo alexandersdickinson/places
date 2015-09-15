7d61eff4cb69e4328487e080310be9c973bec67d
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-09-14 21:22:14 -0700

    Pass first two specs.

---
+++ lib/places.rb
@@ -0,0 +1,14 @@
+class Places
+  def initialize(name, description)
+    @name = name
+    @description = description
+  end
+  
+  def name()
+    @name
+  end
+  
+  def description()
+    @description
+  end
+end
---
+++ spec/places_spec.rb
@@ -0,0 +1,18 @@
+require('rspec')
+require('places')
+
+describe(Places) do
+  describe('#description') do
+    it('returns the place\'s description') do
+      test_place = Places.new('Hawaii', 'This place has a lot of mountains.')
+      expect(test_place.description()).to(eq('This place has a lot of mountains.'))
+    end
+  end
+  
+  describe('#name') do
+    it('returns the name of the place') do
+      test_place = Places.new('Hawaii', 'This part doesn\'t really matter.')
+      expect(test_place.name()).to(eq('Hawaii'))
+    end
+  end
+end
