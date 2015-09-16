01f8742892409c24e87786ebe29b842bf7eb007d
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-09-15 21:03:55 -0700

    Add edit integration spec.

--- spec/places_integration_spec.rb
+++ spec/places_integration_spec.rb
@@ -17,3 +17,24 @@ describe('the places path') do
     expect(page).to(have_content('Hawaii: This is a description. Sarajevo: This is another description.'))
   end
 end
+
+describe('the edit path') do
+  it('allows users to edit entries', {:type => :feature}) do
+    visit('/')
+    fill_in('name', :with => 'Hawaii')
+    fill_in('description', :with => 'This is a description.')
+    click_button('Add')
+    click_link('Back')
+    fill_in('name', :with => 'Sarajevo')
+    fill_in('description', :with => 'This is another description.')
+    click_button('Add')
+    click_link('Back')
+    click_button('Edit')
+    fill_in('entry number', :with => 2)
+    fill_in('name', :with => 'Belgrade')
+    fill_in('description', :with => 'This is a replacement description.')
+    click_button('Edit')
+    click_link('Back')
+    expect(page).to(have_content('Hawaii: This is a description. Belgrade: This is a replacement description.'))
+  end
+end
